fd8b75f968254db6d1fc561118925c6bc7dcfcb8
Alireza Alizadeh <alirezafisher@gmail.com>
2025-10-03 23:15:45 +0330

    1.5

--- ScheduleApp/ScheduleForm.Designer.cs
+++ ScheduleApp/ScheduleForm.Designer.cs
@@ -24,8 +24,22 @@ namespace GuardScheduler
             this.btnGenerateSchedule = new System.Windows.Forms.Button();
             this.dateTimePickerFrom = new PersianDateTimeControl.PersianDatePicker();
             this.dateTimePickerTo = new PersianDateTimeControl.PersianDatePicker();
-            this.dgvSchedule = new System.Windows.Forms.DataGridView();
-            ((System.ComponentModel.ISupportInitialize)(this.dgvSchedule)).BeginInit();
+            this.dgv24HourPosts = new System.Windows.Forms.DataGridView();
+            this.dgvNegahban = new System.Windows.Forms.DataGridView();
+            this.dgvPasbakhsh = new System.Windows.Forms.DataGridView();
+            this.dgvDezhban = new System.Windows.Forms.DataGridView();
+            this.grp24Hour = new System.Windows.Forms.GroupBox();
+            this.grpNegahban = new System.Windows.Forms.GroupBox();
+            this.grpPasbakhsh = new System.Windows.Forms.GroupBox();
+            this.grpDezhban = new System.Windows.Forms.GroupBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv24HourPosts)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvNegahban)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPasbakhsh)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDezhban)).BeginInit();
+            this.grp24Hour.SuspendLayout();
+            this.grpNegahban.SuspendLayout();
+            this.grpPasbakhsh.SuspendLayout();
+            this.grpDezhban.SuspendLayout();
             this.SuspendLayout();
             // 
             // labelFromDate
@@ -33,18 +47,18 @@ namespace GuardScheduler
             this.labelFromDate.AutoSize = true;
             this.labelFromDate.Location = new System.Drawing.Point(14, 19);
             this.labelFromDate.Name = "labelFromDate";
-            this.labelFromDate.Size = new System.Drawing.Size(89, 20);
+            this.labelFromDate.Size = new System.Drawing.Size(58, 20);
             this.labelFromDate.TabIndex = 0;
-            this.labelFromDate.Text = "From Date:";
+            this.labelFromDate.Text = "از تاریخ:";
             // 
             // labelToDate
             // 
             this.labelToDate.AutoSize = true;
             this.labelToDate.Location = new System.Drawing.Point(14, 62);
             this.labelToDate.Name = "labelToDate";
-            this.labelToDate.Size = new System.Drawing.Size(70, 20);
+            this.labelToDate.Size = new System.Drawing.Size(54, 20);
             this.labelToDate.TabIndex = 1;
-            this.labelToDate.Text = "To Date:";
+            this.labelToDate.Text = "تا تاریخ:";
             // 
             // btnGenerateSchedule
             // 
@@ -52,7 +66,7 @@ namespace GuardScheduler
             this.btnGenerateSchedule.Name = "btnGenerateSchedule";
             this.btnGenerateSchedule.Size = new System.Drawing.Size(225, 38);
             this.btnGenerateSchedule.TabIndex = 4;
-            this.btnGenerateSchedule.Text = "Generate Schedule";
+            this.btnGenerateSchedule.Text = "تولید برنامه";
             this.btnGenerateSchedule.UseVisualStyleBackColor = true;
             this.btnGenerateSchedule.Click += new System.EventHandler(this.btnGenerateSchedule_Click);
             // 
@@ -78,36 +92,118 @@ namespace GuardScheduler
             this.dateTimePickerTo.TabIndex = 7;
             this.dateTimePickerTo.Value = null;
             // 
-            // dgvSchedule
+            // dgv24HourPosts
             // 
-            this.dgvSchedule.AllowUserToAddRows = false;
-            this.dgvSchedule.AllowUserToDeleteRows = false;
-            this.dgvSchedule.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
-            | System.Windows.Forms.AnchorStyles.Left)
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.dgvSchedule.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvSchedule.Location = new System.Drawing.Point(17, 150);
-            this.dgvSchedule.Name = "dgvSchedule";
-            this.dgvSchedule.ReadOnly = true;
-            this.dgvSchedule.Size = new System.Drawing.Size(700, 300);
-            this.dgvSchedule.TabIndex = 8;
+            this.dgv24HourPosts.AllowUserToAddRows = false;
+            this.dgv24HourPosts.AllowUserToDeleteRows = false;
+            this.dgv24HourPosts.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv24HourPosts.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgv24HourPosts.Location = new System.Drawing.Point(3, 22);
+            this.dgv24HourPosts.Name = "dgv24HourPosts";
+            this.dgv24HourPosts.ReadOnly = true;
+            this.dgv24HourPosts.Size = new System.Drawing.Size(694, 175);
+            this.dgv24HourPosts.TabIndex = 0;
+            // 
+            // dgvNegahban
+            // 
+            this.dgvNegahban.AllowUserToAddRows = false;
+            this.dgvNegahban.AllowUserToDeleteRows = false;
+            this.dgvNegahban.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvNegahban.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgvNegahban.Location = new System.Drawing.Point(3, 22);
+            this.dgvNegahban.Name = "dgvNegahban";
+            this.dgvNegahban.ReadOnly = true;
+            this.dgvNegahban.Size = new System.Drawing.Size(694, 175);
+            this.dgvNegahban.TabIndex = 0;
+            // 
+            // dgvPasbakhsh
+            // 
+            this.dgvPasbakhsh.AllowUserToAddRows = false;
+            this.dgvPasbakhsh.AllowUserToDeleteRows = false;
+            this.dgvPasbakhsh.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvPasbakhsh.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgvPasbakhsh.Location = new System.Drawing.Point(3, 22);
+            this.dgvPasbakhsh.Name = "dgvPasbakhsh";
+            this.dgvPasbakhsh.ReadOnly = true;
+            this.dgvPasbakhsh.Size = new System.Drawing.Size(694, 175);
+            this.dgvPasbakhsh.TabIndex = 0;
+            // 
+            // dgvDezhban
+            // 
+            this.dgvDezhban.AllowUserToAddRows = false;
+            this.dgvDezhban.AllowUserToDeleteRows = false;
+            this.dgvDezhban.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvDezhban.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgvDezhban.Location = new System.Drawing.Point(3, 22);
+            this.dgvDezhban.Name = "dgvDezhban";
+            this.dgvDezhban.ReadOnly = true;
+            this.dgvDezhban.Size = new System.Drawing.Size(694, 175);
+            this.dgvDezhban.TabIndex = 0;
+            // 
+            // grp24Hour
+            // 
+            this.grp24Hour.Controls.Add(this.dgv24HourPosts);
+            this.grp24Hour.Location = new System.Drawing.Point(17, 150);
+            this.grp24Hour.Name = "grp24Hour";
+            this.grp24Hour.Size = new System.Drawing.Size(700, 200);
+            this.grp24Hour.TabIndex = 8;
+            this.grp24Hour.Text = "پست‌های ۲۴ ساعته";
+            // 
+            // grpNegahban
+            // 
+            this.grpNegahban.Controls.Add(this.dgvNegahban);
+            this.grpNegahban.Location = new System.Drawing.Point(17, 360);
+            this.grpNegahban.Name = "grpNegahban";
+            this.grpNegahban.Size = new System.Drawing.Size(700, 200);
+            this.grpNegahban.TabIndex = 9;
+            this.grpNegahban.Text = "نگهبان";
+            // 
+            // grpPasbakhsh
+            // 
+            this.grpPasbakhsh.Controls.Add(this.dgvPasbakhsh);
+            this.grpPasbakhsh.Location = new System.Drawing.Point(17, 570);
+            this.grpPasbakhsh.Name = "grpPasbakhsh";
+            this.grpPasbakhsh.Size = new System.Drawing.Size(700, 200);
+            this.grpPasbakhsh.TabIndex = 10;
+            this.grpPasbakhsh.Text = "پاس‌بخش";
+            // 
+            // grpDezhban
+            // 
+            this.grpDezhban.Controls.Add(this.dgvDezhban);
+            this.grpDezhban.Location = new System.Drawing.Point(17, 780);
+            this.grpDezhban.Name = "grpDezhban";
+            this.grpDezhban.Size = new System.Drawing.Size(700, 200);
+            this.grpDezhban.TabIndex = 11;
+            this.grpDezhban.Text = "دژبان";
             // 
             // ScheduleForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(740, 480);
-            this.Controls.Add(this.dgvSchedule);
+            this.AutoScroll = true;
+            this.ClientSize = new System.Drawing.Size(1305, 1000);
+            this.Controls.Add(this.grpDezhban);
+            this.Controls.Add(this.grpPasbakhsh);
+            this.Controls.Add(this.grpNegahban);
+            this.Controls.Add(this.grp24Hour);
             this.Controls.Add(this.dateTimePickerTo);
             this.Controls.Add(this.dateTimePickerFrom);
             this.Controls.Add(this.btnGenerateSchedule);
             this.Controls.Add(this.labelToDate);
             this.Controls.Add(this.labelFromDate);
             this.Name = "ScheduleForm";
-            this.Text = "Schedule Generator";
-            ((System.ComponentModel.ISupportInitialize)(this.dgvSchedule)).EndInit();
+            this.Text = "برنامه‌ریزی نگهبانی";
+            ((System.ComponentModel.ISupportInitialize)(this.dgv24HourPosts)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvNegahban)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPasbakhsh)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDezhban)).EndInit();
+            this.grp24Hour.ResumeLayout(false);
+            this.grpNegahban.ResumeLayout(false);
+            this.grpPasbakhsh.ResumeLayout(false);
+            this.grpDezhban.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
+
         }
 
         private System.Windows.Forms.Label labelFromDate;
@@ -115,6 +211,15 @@ namespace GuardScheduler
         private System.Windows.Forms.Button btnGenerateSchedule;
         private PersianDatePicker dateTimePickerFrom;
         private PersianDatePicker dateTimePickerTo;
-        private System.Windows.Forms.DataGridView dgvSchedule;
+
+        private System.Windows.Forms.GroupBox grp24Hour;
+        private System.Windows.Forms.GroupBox grpNegahban;
+        private System.Windows.Forms.GroupBox grpPasbakhsh;
+        private System.Windows.Forms.GroupBox grpDezhban;
+
+        private System.Windows.Forms.DataGridView dgv24HourPosts;
+        private System.Windows.Forms.DataGridView dgvNegahban;
+        private System.Windows.Forms.DataGridView dgvPasbakhsh;
+        private System.Windows.Forms.DataGridView dgvDezhban;
     }
 }
--- ScheduleApp/ScheduleForm.cs
+++ ScheduleApp/ScheduleForm.cs
@@ -34,18 +34,39 @@ namespace GuardScheduler
             dateTimePickerFrom.Value = DateTime.Now;
             dateTimePickerTo.Value = DateTime.Now;
 
-            // Setup DataGridView columns
-            dgvSchedule.Columns.Clear();
-            dgvSchedule.Columns.Add("Date", "تاریخ");
-            dgvSchedule.Columns.Add("Post", "پست");
-            dgvSchedule.Columns.Add("Start", "شروع");
-            dgvSchedule.Columns.Add("Duration", "مدت (ساعت)");
-            dgvSchedule.Columns.Add("Guard", "نگهبان");
+            // Create separate grids for categories
+            SetupGrid(dgv24HourPosts, "پست‌های ۲۴ ساعته");
+            SetupGrid(dgvNegahban, "نگهبان");
+            SetupGrid(dgvPasbakhsh, "پاس‌بخش");
+            SetupGrid(dgvDezhban, "دژبان");
+        }
+
+        private void SetupGrid(DataGridView dgv, string title)
+        {
+            dgv.Columns.Clear();
+            dgv.Columns.Add("Post", "پست");
+
+            // Add columns for each hour of the day
+            for (int hour = 0; hour < 24; hour++)
+            {
+                dgv.Columns.Add($"H{hour}", hour.ToString("00") + ":00");
+            }
+
+            dgv.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.AllCells;
+            dgv.RowHeadersVisible = false;
+            dgv.AllowUserToAddRows = false;
+            dgv.ReadOnly = true;
+
+            // Optional: group box title
+            dgv.Parent.Text = title;
         }
 
         private void btnGenerateSchedule_Click(object sender, EventArgs e)
         {
-            dgvSchedule.Rows.Clear();
+            dgv24HourPosts.Rows.Clear();
+            dgvNegahban.Rows.Clear();
+            dgvPasbakhsh.Rows.Clear();
+            dgvDezhban.Rows.Clear();
 
             DateTime fromDate = dateTimePickerFrom.Value.Value.Date;
             DateTime toDate = dateTimePickerTo.Value.Value.Date;
@@ -56,53 +77,55 @@ namespace GuardScheduler
                 return;
             }
 
-            // Clear old schedule
             _scheduleRepo.DeleteAll();
-
-            // Generate new schedule
             var newSchedule = _schedulerService.GenerateSchedule(fromDate, toDate);
             if (!newSchedule.Any(d => d.ShiftSlots.Any()))
             {
                 MessageBox.Show("هیچ برنامه‌ای تولید نشد.", "خطا", MessageBoxButtons.OK, MessageBoxIcon.Warning);
                 return;
             }
-
-            // Save to repository
             _scheduleRepo.SaveScheduleDays(newSchedule);
 
-            // --- Display schedule in DataGridView ---
+            // --- Display schedule in different timetables ---
             foreach (var day in newSchedule)
             {
                 string jalaliDate = new PersianDateTime(day.Date).ToString("yyyy/MM/dd");
 
-                foreach (var slot in day.ShiftSlots.OrderBy(s => s.Start))
+                // Group slots by post
+                var posts = day.ShiftSlots.GroupBy(s => s.PostId);
+                foreach (var postGroup in posts)
                 {
-                    var post = _postRepo.GetById(slot.PostId);
+                    var post = _postRepo.GetById(postGroup.Key);
+                    if (post == null) continue;
 
-                    // Find all assignments for this slot
-                    var slotAssignments = day.Assignments
-                                             .Where(a => a.ShiftSlotId == slot.Id)
-                                             .ToList();
+                    // Build row for this post
+                    var rowCells = new object[25];
+                    rowCells[0] = $"{post.Name} ({jalaliDate})"; // first cell = post name
 
-                    if (slotAssignments.Any())
+                    foreach (var slot in postGroup)
                     {
-                        foreach (var assignment in slotAssignments)
+                        var assignment = day.Assignments.FirstOrDefault(a => a.ShiftSlotId == slot.Id);
+                        if (assignment != null)
                         {
                             var person = _personRepo.GetById(assignment.PersonId);
-                            string personName = person != null
-                                ? $"{person.FirstName} {person.LastName}"
-                                : "بدون نگهبان";
+                            string personName = person != null ? $"{person.FirstName} {person.LastName}" : "بدون نگهبان";
 
-                            dgvSchedule.Rows.Add(jalaliDate, post?.Name ?? "ناشناس", slot.Start, slot.DurationHours, personName);
+                            int colIndex = slot.Start.Hours; // map hour → column
+                            rowCells[colIndex + 1] = personName;
                         }
                     }
+
+                    // Decide which grid to add row to
+                    if (post.AllowedRoles.Contains(Role.Negahban))
+                        dgvNegahban.Rows.Add(rowCells);
+                    else if (post.AllowedRoles.Contains(Role.PasBakhsh))
+                        dgvPasbakhsh.Rows.Add(rowCells);
+                    else if (post.AllowedRoles.Contains(Role.Dezhban))
+                        dgvDezhban.Rows.Add(rowCells);
                     else
-                    {
-                        dgvSchedule.Rows.Add(jalaliDate, post?.Name ?? "ناشناس", slot.Start, slot.DurationHours, "بدون نگهبان");
-                    }
+                        dgv24HourPosts.Rows.Add(rowCells);
                 }
             }
         }
-
     }
 }
